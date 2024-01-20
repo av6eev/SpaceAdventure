86a4da5385a86d6f79c96d89c9c8b728665c8a03
av6eev <av6eev@gmail.com>
2024-01-20 20:37:11 +0300

    add: main scenes, Startup and GameModel

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Startup : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+    
+    private void Update()
+    {
+    }
+    
+    private void FixedUpdate()
+    {
+    }
+    
+    private void LateUpdate()
+    {
+    }
+
+    private void OnDestroy()
+    {
+    }
+}
